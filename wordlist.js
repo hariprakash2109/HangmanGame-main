99e79586d19aa376a93dda8c09eea8f92a08556d
HARI PRAKASH S <hariprakash2109@gmail.com>
2025-11-06 20:29:59 +0530

    Initial commit

---
+++ wordlist.js
@@ -0,0 +1,45 @@
+
+// Check the source code for the questions
+const wordList = [
+    {
+      word: "dart",
+      hint: "A programming language used to build high-performance mobile or web applications",
+    },
+    {
+      word: "entrophy",
+      hint: "The amount of uncertainty in a system of symbol is called ______",
+    },
+    {
+      word: "firewall",
+      hint: "Which software prevents the external access to a system?",
+    },
+    {
+      word: "topology",
+      hint: "The arrangement of computers and other peripherals in a network is called ______",
+    },
+    {
+      word: "arpanet",
+      hint: "What was the name of the first network?",
+    },
+    {
+      word: "socket",
+      hint: "What is the term for an endpoint of an inter-process communication flow across a computer network?",
+    },
+    {
+      word: "zoho",
+      hint: "Sridhar Vembu is ceo of which company?",
+    },
+    {
+      word: "amazon",
+      hint: "Andy Jassy is ceo of which company?",
+    },
+    {
+      word: "youtube",
+      hint: "Neal Mohan is ceo of?",
+    },
+    {
+      word: "apple",
+      hint: "Tim Cook is ceo of which company?",
+    },
+  ];
+  
